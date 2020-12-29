2853ebcb6ad49892f030904c88c5996b868dcf70
anurag2361 <sharmaanurag.236@gmail.com>
2020-12-29 19:17:09 +0530

    update api

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,59 @@
 extern crate dotenv;
 
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use bson::doc;
-use bson::{DateTime, Document};
+use bson::{doc, oid};
 use chrono::Utc;
 use dotenv::dotenv;
 use futures::stream::StreamExt;
 use mongodb::options::{ClientOptions, StreamAddress};
-use mongodb::{error::Result, Client};
+use mongodb::Client;
 use serde::Deserialize;
 use std::env;
-use std::iter::Iterator;
+use std::sync::*;
 
 // =======Structs============
 #[derive(Deserialize)]
 struct Info {
     name: String,
 }
-
-pub struct State {
-    client: mongodb::Client,
+#[derive(Deserialize)]
+struct Update {
+    id: String,
+    name: String,
 }
-// ==============
+// =========================
 
-async fn index(req: HttpRequest) -> impl Responder {
+async fn index(_req: HttpRequest) -> impl Responder {
     format!("Hello world")
 }
 
-async fn post_request(info: web::Json<Info>, data: web::Data<State>) -> impl Responder {
-    let name: &str = &info.name;
-    let created_at = Utc::now();
-    let updated_at = Utc::now();
-    let collection = data.client.database("test1").collection("users");
-    let result = collection
-        .insert_one(
-            doc! {"name":name,"created_at":created_at,"updated_at":updated_at},
-            None,
-        )
-        .await
-        .unwrap();
+async fn post_request(info: web::Json<Info>, data: web::Data<Mutex<Client>>) -> impl Responder {
+    let document = doc! {
+        "name": info.name.to_string(),
+        "created_at": Utc::now().timestamp_millis(),
+        "updated_at": Utc::now().timestamp_millis(),
+    };
+    let collection = data.lock().unwrap().database("test1").collection("users");
+    let result = collection.insert_one(document, None).await.unwrap();
     HttpResponse::Ok().json(result).await
 }
 
-async fn get_request(req: HttpRequest, data: web::Data<State>) -> impl Responder {
+async fn get_request(req: HttpRequest, data: web::Data<Mutex<Client>>) -> impl Responder {
     let oid = req.match_info().get("oid").expect("OID not found");
-    let collection = data.client.database("test1").collection("users");
+    let collection = data.lock().unwrap().database("test1").collection("users");
     let result = collection
         .find_one(
             doc! {"_id":bson::oid::ObjectId::with_string(oid).unwrap()},
             None,
         )
         .await
+        .unwrap()
         .expect("Error in finding document");
     HttpResponse::Ok().json(result).await
 }
 
-async fn get_all_request(req: HttpRequest, data: web::Data<State>) -> impl Responder {
-    let collection = data.client.database("test1").collection("users");
+async fn get_all_request(_req: HttpRequest, data: web::Data<Mutex<Client>>) -> impl Responder {
+    let collection = data.lock().unwrap().database("test1").collection("users");
     let mut cursor = collection.find(None, None).await.expect("Find error");
     let mut vec = Vec::new();
     while let Some(result) = cursor.next().await {
@@ -66,6 +63,21 @@ async fn get_all_request(req: HttpRequest, data: web::Data<State>) -> impl Respo
         }
     }
     HttpResponse::Ok().json(vec).await
+}
+
+async fn update_doc(body: web::Json<Update>, data: web::Data<Mutex<Client>>) -> impl Responder {
+    let collection = data.lock().unwrap().database("test1").collection("users");
+    let query = doc! {
+        "_id": oid::ObjectId::with_string(&body.id).unwrap()
+    };
+    let update = doc! {
+        "$set": {"name": &body.name,"updated_at": Utc::now().timestamp_millis(), }
+    };
+    let update_result = collection
+        .update_one(query, update, None)
+        .await
+        .expect("Update Error");
+    HttpResponse::Ok().json(update_result).await
 }
 
 fn get_server_address() -> String {
@@ -86,20 +98,19 @@ async fn main() -> std::io::Result<()> {
         }])
         .build();
 
-    let client = Client::with_options(options).unwrap();
+    let client = web::Data::new(Mutex::new(Client::with_options(options).unwrap()));
 
     let port = env::var("PORT").expect("PORT not set.");
     let binding_address = get_server_address();
     println!("Server running at http://127.0.0.1:{}", port);
     HttpServer::new(move || {
         App::new()
-            .data(State {
-                client: client.clone(),
-            })
+            .app_data(client.clone())
             .route("/", web::get().to(index))
             .route("/user/postdata", web::post().to(post_request))
             .route("/user/find/{oid}", web::get().to(get_request))
             .route("/user/getall", web::get().to(get_all_request))
+            .route("/user/update", web::patch().to(update_doc))
     })
     .bind(&binding_address)?
     .run()
